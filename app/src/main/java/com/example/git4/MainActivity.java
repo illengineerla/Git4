21bae2ba5e47bf92cc538f6541f48e38ebf7b627
illengineerla <ill.la.kinin@gmail.com>
2021-12-08 23:04:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/git4/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.git4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.DataInput;
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    private TextView mTextView;
+    private EditText mEditUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mEditUser = (EditText) findViewById(R.id.editText);
+    }
+    public void onClick(View view){
+        String user = mEditUser.getText().toString();
+        mProgressBar.setVisibility(View.VISIBLE);
+        GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+        final Call<GitResult> call = gitHubService.getUsers(user);
+        call.enqueue(new Callback<GitResult>() {
+            @Override
+            public void onResponse(Call<GitResult> call, Response<GitResult> response) {
+                // response.isSuccessful() is true if the response code is 2xx
+                if (response.isSuccessful()) {
+                    GitResult result = response.body();
+                    String user = "Аккаунт Github: " + result.getItems().get(0).getLogin();
+                    mTextView.setText(user);
+                    Log.i("Git", String.valueOf(result.getItems().size()));
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    int statusCode = response.code();
+                    ResponseBody errorBody = response.errorBody();
+                    try {
+                        mTextView.setText(errorBody.string());
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GitResult> call, Throwable throwable) {
+                mTextView.setText("Что-то пошло не так: " + throwable.getMessage());
+            }
+        });
+    }
+}
